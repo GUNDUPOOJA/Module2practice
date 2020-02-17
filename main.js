f5fcde0f9e4effd46e3cd4ab7796b183c0de8b31
Gundu Pooja <gundupooja1995@gmail.com>
2020-02-16 20:42:38 -0600

    changed color and alignment of text

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
-function calculate(){
- let totalIn = parseFloat(document.getElementById("totalIN").value);
- console.log(typeof totalIn);
- let rateIn = parseFloat(document.getElementbyId("rateIN").value);
- if(totalIn == ""|| rateIn == ""){
-     alert("hey you forgot something");
- }
- document.getElementById("tipo").innerHTML = "$"+ totalIn*rateIn;
- document.getElementById("taxo").innerHTML = "$" + totalIn * 0.055;
- document.getElementById("grandtotalo").innerHTML = "$" + (totalIn + (totalIn*rateIn)+ (totalIn*0.055));
-return false;
+function app()
+{
+     const Total = parseFloat(document.querySelector('#total-field').value)
+     const Rate = parseFloat(document.querySelector('#rate-field').value)
+
+     console.log(Rate);
+
+     const Tip = Total*(Rate/100)
+     const TaxRate = 5.5
+     const Tax = Total*(TaxRate/100)
+     const GrandTotal = Total +Tip + Tax;
+
+     document.querySelector('#Tip').innerHTML =`$${Tip.toFixed(2)}`
+     document.querySelector('#Tax').innerHTML=`$${Tax.toFixed(2)}`
+     document.querySelector('#Total').innerHTML=`$${GrandTotal.toFixed(2)}`
 }
-
-
-
-
-
-
-
+     document.querySelector('#calc-Button').addEventListener('click',app)
